c014fac0e5f1aeb35b99c0e10b505d9c4b373ca3
AlexeyPetrochenko <Gyffi-99@yandex.ru>
2024-09-20 13:14:07 +0300

    Сделал 4 из 5 теста, level_2

---
+++ tests/tests_level_2/test_five_replace_word.py
@@ -0,0 +1,6 @@
+import pytest
+from functions.level_2.five_replace_word import replace_word
+
+
+def test__replace_word__():
+    pass
--- tests/tests_level_2/test_one_pr_url.py
+++ tests/tests_level_2/test_one_pr_url.py
@@ -1,5 +1,36 @@
-from functions.level_2 import one_pr_url
+import pytest
+from functions.level_2.one_pr_url import is_github_pull_request_url
 
 
-def test__is_github_pull_request_url__successful_completion():
-    pass
+def test__is_github_pull_request_url__return_true_when_url_is_github_pull_request():
+    assert is_github_pull_request_url('https://github.com/AlexeyPetrochenko/testing_exercises/pull/2')
+    
+
+@pytest.mark.parametrize('invalid_url', 
+                         [
+                            'https://github.com/',
+                            'https://github.ru/AlexeyPetrochenko/testing_exercises/pull/2',
+                            'https://github.com/AlexeyPetrochenko/testing_exercises/push/2',
+                            'https://github.com/AlexeyPetrochenko/testing_exercises',
+                            'https://www.youtube.com/?app=desktop&hl=ru',
+                         ])
+def test__is_github_pull_request_url__return_false_for_invalid_format_urls(invalid_url):
+    assert is_github_pull_request_url(invalid_url) is False
+
+
+
+
+@pytest.mark.parametrize('any_string', 
+                         [
+                             '   ',
+                             '1234',
+                             'hello world'
+                         ])
+def test__is_github_pull_request_url__return_false_for_any_string_that_is_not_url(any_string):
+    assert is_github_pull_request_url(any_string) is False
+
+
+
+def test__is_github_pull_request_url__return_false_for_empty_string():
+    assert is_github_pull_request_url('') is False
+    

5e42498aca98ca3987eec3ac2a182ae4d1ea41da
AlexeyPetrochenko <Gyffi-99@yandex.ru>
2024-09-22 10:14:31 +0300

    Level_2 сделал пятый тест

--- tests/tests_level_2/test_five_replace_word.py
+++ tests/tests_level_2/test_five_replace_word.py
@@ -1,6 +1,23 @@
-import pytest
 from functions.level_2.five_replace_word import replace_word
 
 
-def test__replace_word__():
-    pass
+def test__replace_word__replacement_of_several_words():
+    assert replace_word('Привет Мир Пока Мир', 'Мир', 'World') == 'Привет World Пока World'
+
+
+def test__replace_word__replace_one_word():
+    assert replace_word('Привет Мир', 'Мир', 'World') == 'Привет World'
+    
+    
+def test__replace_word__replacing_words_in_different_registers():
+    assert replace_word('Привет МИР', 'мир', 'World') == 'Привет World'
+    
+
+def test__replace_word__replacing_words_with_punctuation_marks():
+    assert replace_word('Привет, Мир!', 'Мир', 'World') == 'Привет, Мир!'
+    
+
+def test__replace_word__replace_word_in_empty_text():
+    assert replace_word('', 'Мир', 'World') == ''
+    
+    
